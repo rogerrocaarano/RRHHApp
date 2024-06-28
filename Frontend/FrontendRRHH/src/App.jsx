133659b9c238fd622736c51c64cf86c29f3035b6
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-06-28 13:13:03 -0300

    Cambios de SeccionOfert

--- Frontend/FrontendRRHH/src/App.jsx
+++ Frontend/FrontendRRHH/src/App.jsx
@@ -1,9 +1,24 @@
 import { Aside } from "./Components/CommonComponents/Aside";
-
+import { SecOfertas } from "./Components/OfertaDeEmpleo/SecOfertas";
 function App() {
 	return (
-		<main className='w-svw h-svh overflow-x-hidden bg-slate-100'>
+		<main className='w-svw h-svh overflow-x-hidden bg-slate-100 flex'>
 			<Aside />
+			<main className='w-[80%] px-4 py-24'>
+				{/* Secciones de ofertas con scroll  */}
+				<section className='flex flex-col m-4 h-[52%]'>
+					<h2 className='h-fit text-xl font-bold'>
+						Ofertas Pendientes de Aprobacion
+					</h2>
+					<SecOfertas statusOfer='Pending' />
+				</section>
+				<section className='flex flex-col gap-2 m-4 h-[52%]'>
+					<h2 className='h-fit text-xl font-bold'>
+						Ofertas Pendientes de Aprobacion
+					</h2>
+					<SecOfertas statusOfer='Active' />
+				</section>
+			</main>
 		</main>
 	);
 }
