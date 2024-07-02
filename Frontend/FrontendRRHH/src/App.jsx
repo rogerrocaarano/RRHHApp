cbdf9279f759ffd4c993f4c479644b4367be633e
port86 <rodriguez.fernando011@gmailcom>
2024-07-02 10:37:03 -0300

    aca no hice nada, le rrer de rara y me estoy queriendo cambair

--- Frontend/FrontendRRHH/src/App.jsx
+++ Frontend/FrontendRRHH/src/App.jsx
@@ -1,6 +1,7 @@
 import { Aside } from "./Components/CommonComponents/Aside";
 import { Navbar } from "./Components/CommonComponents/Navbar.1";
 import { OfertView } from "./Components/OfertaDeEmpleo/Index";
+
 function App() {
 	return (
 		<main className='w-screen h-screen overflow-x-hidden bg-slate-100 flex flex-col'>
@@ -8,6 +9,7 @@ function App() {
 			<main className='flex h-full'>
 				<Aside />
 				<OfertView />
+        
 			</main>
 		</main>
 	);
