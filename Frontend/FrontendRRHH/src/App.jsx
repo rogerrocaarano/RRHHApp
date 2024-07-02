67857e719575fe9c2f8b4da480903eef7d330e8f
port86 <rodriguez.fernando011@gmailcom>
2024-07-02 10:00:26 -0300

    Cree barra buscadora

--- Frontend/FrontendRRHH/src/App.jsx
+++ Frontend/FrontendRRHH/src/App.jsx
@@ -1,11 +1,14 @@
 import { Aside } from "./Components/CommonComponents/Aside";
 import { Navbar } from "./Components/CommonComponents/Navbar.1";
+import { Buscador } from "./Components/CommonComponents/Buscador";
 
 function App() {
   return (
     <main className="w-svw h-svh overflow-x-hidden bg-slate-100">
       <Navbar />
       <Aside />
+      <Buscador />
+       
     </main>
   );
 }
