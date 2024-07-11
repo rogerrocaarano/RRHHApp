5b10d014e4d962596ae4b7379bad65ee05a16008
Róger Roca Arano <63246718+rogerrocaarano@users.noreply.github.com>
2024-07-10 22:51:38 -0400

    feat: Implementación Identity

--- RRHHApp.Api/Program.cs
+++ RRHHApp.Api/Program.cs
@@ -1,38 +1,50 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using RRHHApp.Api.Application.Interfaces;
 using RRHHApp.Api.Application.Services;
 using RRHHApp.Api.Domain.Entities;
 using RRHHApp.Api.Domain.Repositories;
 using RRHHApp.Api.Domain.Services;
+using RRHHApp.Api.Infraestructure.ExternalServices.MailKit;
 using RRHHApp.Api.Infraestructure.Persistence;
 using RRHHApp.Api.Infraestructure.Persistence.EF;
 using RRHHApp.Api.Infraestructure.Persistence.Memory;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Builder configuration sources
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables("DOTNET_");
+
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-// Add repositories
-builder.Services.AddDbContext<AppDbContext>(options => 
+
+
+// Email
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+builder.Services.AddTransient<IEmailSender, MailService>();
+
+builder.Services.AddIdentity<User, UserRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-builder.Services.AddIdentity<User, UserRole>(options => 
-        options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
-
+// Add repositories
 builder.Services.AddScoped<IJobOfferRepository, EfJobOfferRepository>();
 builder.Services.AddScoped<IJobOfferReviewRepository, MemJobOfferReviewRepository>();
 
-// Add domain services
-builder.Services.AddScoped<JobOfferService>();
+// Ensure domain services are added only once
 builder.Services.AddScoped<JobOfferService>();
 
 // Add Application services
@@ -67,5 +79,6 @@ if (app.Environment.IsDevelopment())
 app.UseCors("AllowSpecificOrigin");
 app.MapControllers();
 app.UseHttpsRedirection();
+app.MapIdentityApi<User>();
 
 app.Run();
