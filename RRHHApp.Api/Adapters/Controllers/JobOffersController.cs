ab24d3f7bc897645e3082ed8cd504a0fbf00414d
Róger Roca Arano <63246718+rogerrocaarano@users.noreply.github.com>
2024-07-13 12:19:21 -0400

    feat: Protección de endpoints
    Se añade validación de roles a los endpoints del controlador JobOffersController.cs

--- RRHHApp.Api/Adapters/Controllers/JobOffersController.cs
+++ RRHHApp.Api/Adapters/Controllers/JobOffersController.cs
@@ -1,52 +1,95 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RRHHApp.Api.Application.DTOs;
 using RRHHApp.Api.Application.Interfaces;
-using RRHHApp.Api.Domain.Entities;
 
 namespace RRHHApp.Api.Adapters.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
+[Authorize]
 public class JobOffersController(IJobOfferAppService jobOfferAppService) : ControllerBase
 {
     private readonly IJobOfferAppService _jobOfferAppService = jobOfferAppService;
-    
+
     [HttpGet]
     public async Task<ActionResult> GetJobOffers()
     {
-        var jobOffers = await _jobOfferAppService.GetJobOffers();
+        List<JobOfferDto> jobOffers;
+        if (User.IsInRole("Candidate"))
+        {
+            // obtener solamente las ofertas publicadas y no vencidas
+            jobOffers = await _jobOfferAppService.GetPublishedJobOffers();
+        }
+        else
+        {
+            jobOffers = await _jobOfferAppService.GetJobOffers();
+        }
+
         return Ok(jobOffers);
     }
-    
+
     [HttpGet("{id}")]
     public async Task<ActionResult> GetJobOffer(Guid id)
     {
-        var jobOffer = await _jobOfferAppService.GetJobOffer(id);
+        JobOfferDto jobOffer;
+        if (User.IsInRole("Candidate"))
+        {
+            // mostrar solamente si la oferta está publicada y no vencida
+            jobOffer = await _jobOfferAppService.GetPublishedJobOffer(id);
+        }
+        else
+        {
+            jobOffer = await _jobOfferAppService.GetJobOffer(id);
+        }
+
         return Ok(jobOffer);
     }
-    
+
     [HttpGet("{id}/Requirements")]
     public async Task<ActionResult> GetJobOfferRequirements(Guid id)
     {
-        var requirements = await _jobOfferAppService.GetJobOfferRequirements(id);
+        List<JobOfferRequirementDto> requirements;
+        if (User.IsInRole("Candidate"))
+        {
+            // mostrar solamente si la oferta está publicada y no vencida
+            requirements = await _jobOfferAppService.GetJobOfferRequirements(id);
+        }
+        else
+        {
+            requirements = await _jobOfferAppService.GetJobOfferRequirements(id);
+        }
+
         return Ok(requirements);
     }
-    
+
     [HttpGet("{id}/Requirements/{requirementId}")]
     public async Task<ActionResult> GetJobOfferRequirement(Guid id, Guid requirementId)
     {
-        var requirement = await _jobOfferAppService.GetJobOfferRequirement(id, requirementId);
+        JobOfferRequirementDto requirement;
+        if (User.IsInRole("Candidate"))
+        {
+            // mostrar solamente si la oferta está publicada y no vencida
+            requirement = await _jobOfferAppService.GetJobOfferRequirement(id, requirementId);
+        }
+        else
+        {
+            requirement = await _jobOfferAppService.GetJobOfferRequirement(id, requirementId);
+        }
+
         return Ok(requirement);
     }
-    
+
     [HttpPost("Create")]
+    [Authorize(Roles = "Admin,Director,Recruiter")]
     public async Task<ActionResult> CreateJobOffer([FromBody] CreateJobOfferDto createJobOfferDto)
     {
         await _jobOfferAppService.CreateJobOffer(createJobOfferDto);
         return Ok();
     }
-    
+
     [HttpPost("AddRequirement")]
+    [Authorize(Roles = "Admin,Director,Recruiter")]
     public async Task<ActionResult> AddRequirement([FromBody] CreateJobOfferRequirementDto jobOfferRequirementDto)
     {
         await _jobOfferAppService.AddRequirement(jobOfferRequirementDto);
--- RRHHApp.Api/Domain/Services/JobOfferService.cs
+++ RRHHApp.Api/Domain/Services/JobOfferService.cs
@@ -40,10 +40,31 @@ public class JobOfferService(IJobOfferRepository jobOfferRepository)
         return jobOffer;
     }
 
+    public async Task<JobOffer> GetPublishedJobOffer(Guid id)
+    {
+        var jobOffer = await GetJobOffer(id);
+        if (jobOffer.PublishedDate == null || jobOffer.ExpirationDate > DateTime.Now)
+        {
+            throw new Exception("Job offer not published or expired");
+        }
+
+        return jobOffer;
+    }
+
     public async Task<List<JobOffer>> GetAllJobOffers()
     {
         var jobOffers = await _jobOfferRepository.GetAll();
         return jobOffers;
+    }
+
+    public async Task<List<JobOffer>> GetPublishedJobOffers()
+    {
+        var jobOffers = await GetAllJobOffers();
+        var publishedJobOffers = jobOffers.Where(jo =>
+                jo.PublishedDate != null &&
+                jo.ExpirationDate > DateTime.Now)
+            .ToList();
+        return publishedJobOffers;
     }
 
     public async Task<JobRequirement> GetJobOfferRequirement(Guid requirementId)
@@ -52,9 +73,33 @@ public class JobOfferService(IJobOfferRepository jobOfferRepository)
         return requirement;
     }
 
+    public async Task<JobRequirement> GetJobOfferPublishedRequirement(Guid requirementId)
+    {
+        var requirement = await GetJobOfferRequirement(requirementId);
+        var jobOffer = await GetPublishedJobOffer(requirement.JobOfferId);
+        if (jobOffer == null)
+        {
+            throw new Exception("Job offer not published or expired");
+        }
+
+        return requirement;
+    }
+
     public async Task<List<JobRequirement>> GetAllJobOfferRequirements(Guid jobOfferId)
     {
         var requirements = await _jobOfferRepository.GetAllRequirements(jobOfferId);
+        return requirements;
+    }
+
+    public async Task<List<JobRequirement>> GetPublishedJobOfferRequirements(Guid jobOfferId)
+    {
+        var requirements = await GetAllJobOfferRequirements(jobOfferId);
+        var jobOffer = await GetPublishedJobOffer(jobOfferId);
+        if (jobOffer == null)
+        {
+            throw new Exception("Job offer not published or expired");
+        }
+
         return requirements;
     }
 
@@ -70,6 +115,7 @@ public class JobOfferService(IJobOfferRepository jobOfferRepository)
         {
             throw new Exception("Job offer not found");
         }
+
         var requirement = await _jobOfferRepository.AddRequirement(jobRequirement);
         return await GetJobOffer(requirement.JobOfferId);
     }
